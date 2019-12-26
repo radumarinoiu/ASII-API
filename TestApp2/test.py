5e405ed07779f796634f7d26009c5b5dc4306aa5
Radu-Florin Marinoiu <radumarinoiu98@gmail.com>
2019-12-27 00:18:44 +0200

    Removed recursive loop between services

--- TestApp2/test.py
+++ TestApp2/test.py
@@ -1,5 +1,3 @@
-import requests
-
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 
@@ -9,8 +7,7 @@ CORS(app)
 
 @app.route("/test", methods=["GET"])
 def test():
-    resp = requests.get("http://testapp1-service:80/test")
-    return jsonify({"response": "Service 2 Works!", "additional_data": resp.json()})
+    return jsonify({"response": "Service 2 Works!"})
 
 
 if __name__ == "__main__":

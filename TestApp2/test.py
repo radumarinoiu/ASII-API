057c828c56bdcb45f5df32c7519567a2d6da2903
Radu-Florin Marinoiu <radumarinoiu98@gmail.com>
2019-12-26 16:26:54 +0200

    Added 2 more test projects

---
+++ TestApp2/test.py
@@ -0,0 +1,19 @@
+import json
+
+import requests
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/test", methods=["GET"])
+def test():
+    resp = requests.get("http://10.107.72.180/test")
+    return jsonify({"response": "Service 2 Works!", "additional_data": resp.json()})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+    app.debug = True

bafa00cc9ae9f6ebbe082f07ec6af4f603e68ae7
Radu-Florin Marinoiu <radumarinoiu98@gmail.com>
2020-08-22 20:45:16 +0300

    Basic Demo OAuth client is mostly done.

--- ASII_OAuth_Client/app.py
+++ ASII_OAuth_Client/app.py
@@ -1,7 +1,10 @@
+import logging
+import time
+from http import HTTPStatus
 from secrets import token_hex
 
 import requests
-from flask import Flask, Blueprint, jsonify, redirect, request, session
+from flask import Flask, Blueprint, jsonify, redirect, request, session, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -20,17 +23,59 @@ def setup(client_id, client_secret, scopes, authorization_uri, access_token_uri)
         "scopes": " ".join(scopes),
         "grant_type": "authorization_code",
         "response_type": "code",
-        "api_url": "http://localhost:5000/api/me"
+        "api_url": "http://localhost:5000/"
     }
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
+
+
+def get_user_data(url):
+    access_token = session.get("access_token")
+    if access_token:
+        resp = requests.get(app_data["api_url"] + url, headers={"Authorization": "Bearer {}".format(access_token)})
+        if resp.status_code == 200:
+            return resp.json()
+    return None
+
+
+@app.route("/profile_basic", methods=["GET"])
+def get_basic_profile():
+    user_data = get_user_data("/api/me")
+    if user_data is not None:
+        return render_template("profile.html", profile_data=user_data)
+    if time.time() - session.get("access_token_time", 0) < 5:
+        return jsonify({"err": "Permission denied!"}), HTTPStatus.UNAUTHORIZED
+    session["next_url"] = "/profile_basic"
+    return redirect("/authorize")
+
+
+@app.route("/profile", methods=["GET"])
+def get_profile():
+    user_data = get_user_data("/profile")
+    if user_data is not None:
+        return render_template("profile.html", profile_data=user_data)
+    if time.time() - session.get("access_token_time", 0) < 5:
+        return jsonify({"err": "Permission denied!"}), HTTPStatus.UNAUTHORIZED
+    session["next_url"] = "/profile"
+    return redirect("/authorize")
+
+
+@app.route("/member", methods=["GET"])
+def get_member_data():
+    user_data = get_user_data("/members")
+    if user_data is not None:
+        return render_template("error.html", error=user_data)
+    if time.time() - session.get("access_token_time", 0) < 5:
+        return jsonify({"err": "Permission denied!"}), HTTPStatus.UNAUTHORIZED
+    session["next_url"] = "/member"
+    return redirect("/authorize")
 
 
 @app.route("/authorize", methods=["GET"])
 def authorize():
-    # access_token = session.get("access_token")
-    # if access_token:
-    #     resp = requests.get(app_data["api_url"], headers={"Authorization": "Bearer {}".format(access_token)})
-    #     if resp.status_code == 200:
-    #         return jsonify(resp.json())
     state_val = token_hex(16)
     session["state_val"] = state_val
     return redirect(
@@ -75,8 +120,8 @@ def auth_callback():
     """
     authorization_code = request.args.get("code")
     state_val = request.args.get("state")
-    # if state_val != session.get("state_val"):
-    #     return "Wrong state value. This could be a Man-in-the-middle attack!", 401
+    if state_val != session.get("state_val"):
+        return "Wrong state value. This could be a Man-in-the-middle attack!", 401
     resp = requests.post(app_data["access_token_uri"],
                          data={
                              "grant_type": "authorization_code",
@@ -86,17 +131,24 @@ def auth_callback():
                              app_data["client_id"], app_data["client_secret"]))
     access_token = resp.json().get("access_token")
     session["access_token"] = access_token
-    resp = requests.get(app_data["api_url"], headers={"Authorization": "Bearer {}".format(access_token)})
-    resp_dict = resp.json()
-    resp_dict["test"] = True
-    return jsonify(resp_dict)
+    session["access_token_time"] = time.time()
+    next_url = session.pop("next_url", "/")
+    # try:
+    #     resp = requests.get(request.url_root + next_url)
+    #     if resp.status_code != 200:
+    #         logging.error("Permission denied for resource [{}]".format(request.url_root + next_url))
+    #         return render_template("error.html", error="Permission denied for resource [{}]".format(request.url_root + next_url))
+    # except Exception:
+    #     logging.exception("Failed to access resource [{}]".format(request.url_root + next_url))
+    #     return render_template("error.html", error="Failed to access resource [{}]".format(request.url_root + next_url))
+    return redirect(next_url)
 
 
 if __name__ == '__main__':
     setup(
-        client_id="mDcZGr5fl8Q8Fa1jNjjq6X5p",
-        client_secret="T8bHDeGt0YPfsnQAJy3FGyXCchpQNBDnIZCuNO7D4u4srKLo",
-        scopes=["profile"],
+        client_id="LEVyjBkKu4g0q2fqGWsxsTAi",
+        client_secret="veHQSPugf701fx6CkP88hRXVkMfF8xwtMAEWSTl0WiS45zGg",
+        scopes="profile profile_read asii_members_read fiipractic".split(" "),
         authorization_uri="http://localhost:5000/oauth/authorize",
         access_token_uri="http://localhost:5000/oauth/token"
     )
